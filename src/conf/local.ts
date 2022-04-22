7ec1e80549e491bf942c832b9d69000628fb554b
Kaleb Mills <kaleb@Kalebs-MacBook-Pro.local>
2022-04-21 22:39:06 -0500

    Small code cleanup

--- src/conf/local.ts
+++ src/conf/local.ts
@@ -6,15 +6,5 @@ export default function LocalConf(): ServiceOptions {
         name: 'Keeper'
     })
 
-    // const slackClient = new SlackClient({
-    //     port: 3000,
-    //     signingSecret: getEnv(ENV.SLACK_SIGNING_SECRET),
-    //     token: getEnv(ENV.SLACK_BOT_TOKEN),
-    //     appToken: getEnv(ENV.SLACK_APP_TOKEN),
-    //     logger,
-    // })
-
-    // const keeperClient = new KeeperClient({ logger, slackClient })
-
     return { clients: [], logger }
 }
--- src/lib/clients/phony.ts
+++ src/lib/clients/phony.ts
@@ -1,11 +1,21 @@
+import { BaseOptions } from "../../types/generic";
 import { Client } from "../../types/generic";
+import { Logger } from '../../types/logger'
 
 export default class PhonyClient implements Client {
+    private logger: Logger;
+
+    constructor (options: BaseOptions) {
+        this.logger = options.logger;
+    }
+
     start(): Promise<void> {
+        this.logger.log(`${this.constructor.name}.start():SUCCESS`)
         return Promise.resolve();
     }
 
     stop(): Promise<void> {
-        return Promise.reject();
+        this.logger.log(`${this.constructor.name}.stop():SUCCESS`)
+        return Promise.resolve();
     }
 }
